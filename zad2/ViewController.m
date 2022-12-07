0f551eb1fbf57fe4bc3002f103bf9d01be74d03c
Szymon N <sniescior160@gmail.com>
2022-12-07 16:33:56 +0100

    Initial app

--- zad2/ViewController.m
+++ zad2/ViewController.m
@@ -13,10 +13,20 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void) viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [_informationButton setTitle:@"Information" forState:UIControlStateNormal];
 }
 
+- (IBAction) informationButtonPressed {
+    UIAlertController *alertDialog = [UIAlertController alertControllerWithTitle:@"Information" message:[NSString stringWithFormat:@"The faculty is running %i programs at graduate and undergraduate level.", 4] preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){}];
+    
+    [alertDialog addAction:defaultAction];
+    [self presentViewController:alertDialog animated:YES completion:nil];
+}
 
 @end
